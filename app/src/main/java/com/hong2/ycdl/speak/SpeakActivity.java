294e23b9091389b6b971274f87958e0ab2a11315
hong-pyo <ghdvy010@gmail.com>
2019-03-12 01:04:06 +0900

    policy controller

--- app/src/main/java/com/hong2/ycdl/speak/SpeakActivity.java
+++ app/src/main/java/com/hong2/ycdl/speak/SpeakActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -14,12 +15,14 @@ import android.widget.TextView;
 import com.hong2.ycdl.R;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpeakActivity extends Activity {
 
     private SpeechRecognizer mRecognizer;
     private Intent intent;
-    private TextView speak_view;
+    private TextView speak_view, policy_text_view;
     private Button voice_btn;
     private LinearLayout backgroundColor;
 
@@ -27,10 +30,21 @@ public class SpeakActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speak);
-
+        policy_text_view = findViewById(R.id.audio_policy);
         speak_view = findViewById(R.id.speak_view_text);
         backgroundColor = findViewById(R.id.speak_background_image_view);
         voice_btn = findViewById(R.id.speak_voice_btn);
+
+        Linkify.TransformFilter transformFilter = new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher match, String url) {
+                return "";
+            }
+        };
+
+        Pattern pattern = Pattern.compile("오디오 권한 정책");
+
+        Linkify.addLinks(policy_text_view, pattern, "http://52.78.131.64:8080/policy/audio", null, transformFilter);
 
         voice_btn.setOnClickListener(new View.OnClickListener() {
             @Override
